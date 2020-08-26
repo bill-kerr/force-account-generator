861d019b62aa01c674c333aa88d4067217b45515
Bill Kerr <48515765+bk7987@users.noreply.github.com>
2020-08-26 16:16:08 -0400

    PDF generation stages working

--- force_account_generator/webapp/static/webapp/main.js
+++ force_account_generator/webapp/static/webapp/main.js
@@ -8,9 +8,12 @@ function initDropzone() {
     maxFiles: 1,
     init: function () {
       this.hiddenFileInput.removeAttribute("multiple");
-      this.on("addedfile", onAddedFile);
+      this.on("processing", onProcessing);
       this.on("uploadprogress", onUploadProgress);
       this.on("maxfilesexceeded", onMaxFilesExceeded);
+      this.on("success", onUploadSuccess);
+      this.on("sending", onSending);
+      this.on("error", onUploadError);
     },
     paramName: "docfile",
     createImageThumbnails: false,
@@ -21,16 +24,28 @@ function initDropzone() {
 }
 initDropzone();
 
-const initialState = {
+const appData = {
+  fileAdded: false,
   uploading: false,
   fileBytes: 0,
   bytesSent: 0,
   fileSizeUnit: "KB",
+  error: null,
+  stage: 0,
+  currentPage: 0,
+  totalPages: 0,
 
-  setUploading({ fileBytes }) {
-    this.uploading = true;
-    this.fileBytes = fileBytes;
-    this.fileSizeUnit = fileBytes / 1000 >= 1000 ? "MB" : "KB";
+  get fileSize() {
+    return this._getSize(this.fileBytes);
+  },
+
+  get transferredSize() {
+    return this._getSize(Math.min(this.bytesSent, this.fileBytes));
+  },
+
+  get transferredPercent() {
+    const percent = Math.min((this.bytesSent / this.fileBytes) * 100, 100);
+    return percent.toString() + "%";
   },
 
   _getSize(bytes) {
@@ -40,26 +55,53 @@ const initialState = {
     return (Math.round((bytes / 1000000) * 100) / 100).toFixed(2);
   },
 
-  get fileSize() {
-    return this._getSize(this.fileBytes);
+  reset() {
+    this.error = null;
+    this.fileAdded = false;
+    this.uploading = false;
+    this.fileBytes = 0;
+    this.bytesSent = 0;
+    this.fileSizeUnit = "KB";
+    this.stage = 0;
+    this.currentPage = 0;
+    this.totalPages = 0;
   },
 
-  get transferredSize() {
-    return Math.min(this._getSize(this.bytesSent), this.fileBytes);
+  setUploading({ fileBytes }) {
+    this.fileAdded = true;
+    this.uploading = true;
+    this.error = null;
+    this.fileBytes = fileBytes;
+    this.fileSizeUnit = fileBytes / 1000 >= 1000 ? "MB" : "KB";
   },
 
-  get transferredPercent() {
-    const percent = Math.min((this.bytesSent / this.fileBytes) * 100, 100);
-    return percent.toString() + "%";
+  setStage(stage) {
+    this.stage = stage;
+    if (this.stage >= 3 && this.currentPage !== this.totalPages) {
+      this.currentPage = this.totalPages;
+    }
   },
 };
 
-function onAddedFile(file) {
-  window.dispatchEvent(new CustomEvent("addedfile", { detail: { fileBytes: file.size } }));
+function onProcessing(file) {
+  window.dispatchEvent(new CustomEvent("processingupload", { detail: { fileBytes: file.size } }));
 }
 
 function onUploadProgress(_file, progress, bytesSent) {
   window.dispatchEvent(new CustomEvent("uploadprogress", { detail: { progress, bytesSent } }));
+}
+
+function onUploadSuccess(_file, response) {
+  window.dispatchEvent(new CustomEvent("uploadsuccess"));
+  if (response["task_id"]) {
+    return initCeleryProgress(response["task_id"]);
+  }
+  dispatchError("No response from the server. Please try again in a little bit.");
+}
+
+function onUploadError(_file, errorMessage) {
+  this.removeAllFiles(true);
+  dispatchError(errorMessage);
 }
 
 function onMaxFilesExceeded(file) {
@@ -67,36 +109,49 @@ function onMaxFilesExceeded(file) {
   this.addFile(file);
 }
 
-var stage1 = document.getElementById("stage-1");
-var stage2 = document.getElementById("stage-2");
-var stage3 = document.getElementById("stage-3");
+function onSending(_file, _xhrObj, formData) {
+  const dailySheets = document.getElementById("daily_sheets").checked;
+  formData.append("daily_sheets", dailySheets);
+}
 
 function initCeleryProgress(taskId) {
   const progressUrl = "/celery-progress/" + taskId;
-  console.log(progressUrl);
   CeleryProgressBar.initProgressBar(progressUrl, {
-    onProgress: onProgress,
-    onSuccess: onSuccess,
-    onError: onError,
+    onProgress: onGenerateProgress,
+    onSuccess: onGenerateSuccess,
+    onError: onGenerateError,
   });
 }
 
-function onProgress(_progElem, _msgElem, { description: progress }) {
+function onGenerateProgress(_progElem, _msgElem, { description: progress }) {
   if (!progress) {
     return;
   }
+  dispatchSetStage(progress.stage);
+
+  console.log(progress);
+
+  if (progress.stage === 2) {
+    const current = progress["stage_progress"];
+    const total = progress["stage_total"];
+    window.dispatchEvent(new CustomEvent("pdfprogress", { detail: { current, total } }));
+  }
 }
 
-function showAlpine() {
-  return false;
+function onGenerateSuccess(_progElem, _msgElem, _result) {
+  dispatchSetStage(4);
 }
 
-function onSuccess(_progElem, _msgElem, result) {
-  //console.log(result);
+function onGenerateError(_progElem, _msgElem, _errorMessage) {
+  dispatchError("An error occurred while processing the Excel file. Please make sure it is a valid template.");
 }
 
-function onError(_progElem, _msgElem, errorMessage) {
-  //console.error(errorMessage);
+function dispatchError(errorMessage) {
+  window.dispatchEvent(new CustomEvent("error", { detail: { error: errorMessage } }));
+}
+
+function dispatchSetStage(stage) {
+  window.dispatchEvent(new CustomEvent("setstage", { detail: { stage } }));
 }
 
 // function setStage(stage) {
